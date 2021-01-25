ca6748d061e7b322afd0a333bedd024d54026135
Ben <46476247+hexagonatron@users.noreply.github.com>
2021-01-25 23:52:58 +1030

    Readme tweaks and deploy tweaks

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Simple code to test the functionality of the spotify SDK web player
 
+## [Test it here!](https://hexagonatron.github.io/SpotiTest)
+
 - In order to test this page, create a (free) developer account on spotify and obtain a client Id and client secret
-- Fill them into the boxes provided and press the button
+- Edit the app setting in the spotify dashboard and add https://hexagonatron.github.io/SpotiTest as a redirect uri
+- Fill client id and client secret into the boxes provided and press the button
 - This only works if you authenticate with a Spotify premium accounts
+- Enjoy the music!
--- script.js
+++ script.js
@@ -1,17 +1,79 @@
-const SPOTIFY_CLIENT_ID = "d6a905c9a88a47a5a6d0d181cfae18bb";
-const REDIRECT = window.location.href;
-
 window.onSpotifyWebPlaybackSDKReady = () => {
+    let REDIRECT = window.location.href;
+    if (REDIRECT[REDIRECT.length - 1] === "/") {
+        REDIRECT = window.location.href.slice(0, -1);
+    }
+
+    console.log(REDIRECT);
+
+
+    const exchangeCodeForToken = (code) => {
+
+        const spotify_client_secret = document.querySelector("#client-secret").value;
+        const spotify_client_id = document.querySelector("#client-id").value;
+
+        const base64EncodedCreds = btoa(`${spotify_client_id}:${spotify_client_secret}`);
+        const authHeader = `Basic ${base64EncodedCreds}`;
+
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", authHeader);
+        myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+        const urlencoded = new URLSearchParams();
+        urlencoded.append("grant_type", "authorization_code");
+        urlencoded.append("code", code);
+        urlencoded.append("redirect_uri", REDIRECT);
+
+        const requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: urlencoded,
+            redirect: 'follow'
+        };
+
+        return fetch("https://accounts.spotify.com/api/token", requestOptions)
+            .then(response => response.json())
+            .then(result => result.access_token)
+            .catch(error => console.log('error', error));
+    }
+
+    const playSong = (device_id) => {
+
+        //Get token out of local storage
+        const token = localStorage.getItem("token");
+
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", `Bearer ${token}`);
+        myHeaders.append("Content-Type", "application/json");
+
+        //Set the song to play. Also make sure the volume is up.
+        var raw = JSON.stringify({ "uris": ["spotify:track:4uLU6hMCjMI75M1A2tKUQC"], "volume": 100 });
+
+        var requestOptions = {
+            method: 'PUT',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        //Call the 'play' endpoint
+        fetch("https://api.spotify.com/v1/me/player/play?device_id=116aca340895499734d0fc05be7ff7deaec8957d", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+    }
+
+
     const player = new Spotify.Player({
         name: 'Web Playback SDK Quick Start Player',
         getOAuthToken: cb => {
-            
+
             //This code is to get the users oAuth Token
             document.querySelector('#button').addEventListener("click", (e) => {
                 e.preventDefault();
 
                 //App client Id and required scopes
-                const client_id = SPOTIFY_CLIENT_ID;
+                const client_id = document.querySelector("#client-id").value;
                 const scopes = ['app-remote-control', 'user-modify-playback-state', 'user-read-currently-playing', 'user-read-playback-position', 'streaming', 'user-read-recently-played', 'user-read-playback-state', "streaming", "user-read-email", "user-read-private"];
 
                 //Open a popup window to ask user to verify with Spotify. Save a ref to the popup in a variable so we can query it later
@@ -78,59 +140,3 @@ window.onSpotifyWebPlaybackSDKReady = () => {
     // Connect to the player!
     player.connect();
 };
-
-
-const exchangeCodeForToken = (code) => {
-
-    const spotify_client_secret = document.querySelector("#client-secret").value;
-
-    const base64EncodedCreds = btoa(`${SPOTIFY_CLIENT_ID}:${spotify_client_secret}`);
-    const authHeader = `Basic ${base64EncodedCreds}`;
-
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", authHeader);
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-
-    const urlencoded = new URLSearchParams();
-    urlencoded.append("grant_type", "authorization_code");
-    urlencoded.append("code", code);
-    urlencoded.append("redirect_uri", REDIRECT);
-
-    const requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: urlencoded,
-        redirect: 'follow'
-    };
-
-    return fetch("https://accounts.spotify.com/api/token", requestOptions)
-        .then(response => response.json())
-        .then(result => result.access_token)
-        .catch(error => console.log('error', error));
-}
-
-const playSong = (device_id) => {
-
-    //Get token out of local storage
-    const token = localStorage.getItem("token");
-
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", `Bearer ${token}`);
-    myHeaders.append("Content-Type", "application/json");
-
-    //Set the song to play. Also make sure the volume is up.
-    var raw = JSON.stringify({ "uris": ["spotify:track:4uLU6hMCjMI75M1A2tKUQC"], "volume": 100 });
-
-    var requestOptions = {
-        method: 'PUT',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
-
-    //Call the 'play' endpoint
-    fetch("https://api.spotify.com/v1/me/player/play?device_id=116aca340895499734d0fc05be7ff7deaec8957d", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-}
